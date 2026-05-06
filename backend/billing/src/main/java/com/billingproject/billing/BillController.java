23ce83dac5ba58ea7bacb5e6462bce8a270fedf4
Saicharan <saicharan.t27@gmail.com>
2026-05-06 12:09:41 +0530

    Configured H2 DB

--- backend/billing/src/main/java/com/billingproject/billing/BillController.java
+++ backend/billing/src/main/java/com/billingproject/billing/BillController.java
@@ -12,19 +12,19 @@ public class BillController {
     @Autowired
     private BillService service;
 
-    // CREATE
+
     @PostMapping
     public Bill create(@RequestBody Bill bill) {
         return service.createBill(bill);
     }
 
-    // GET ALL
+
     @GetMapping
     public List<Bill> getAll() {
         return service.getAllBills();
     }
 
-    // GET BY ID
+
     @GetMapping("/{id}")
     public Bill getOne(@PathVariable Long id) {
         return service.getBill(id);
